1b2a5240e37067515c81fd566de90f5fd28eb3ed
denis96z <zinoviev.denis@yandex.ru>
2018-05-28 23:52:40 +0300

    Client: GUI changes

--- client/mainwindow.py
+++ client/mainwindow.py
@@ -1,7 +1,22 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, QtGui, uic
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         uic.loadUi('mainwindow.ui', self)
+        self.miExit.triggered.connect(QtWidgets.qApp.quit)
+        self.miOpen.triggered.connect(self.open_file)
+        self.btnLoadImage.clicked.connect(self.open_file)
+
+    def open_file(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(parent=self, caption='Выбор изображения ГРЗ',
+                                                     filter='Файл изображения (*.png)')
+        if not file:
+            return
+        filename = file[0]
+        self.tbImagePath.setText(filename)
+        img = QtGui.QPixmap(filename).scaledToWidth(self.gvImage.width())
+        scene = QtWidgets.QGraphicsScene()
+        scene.addItem(QtWidgets.QGraphicsPixmapItem(img))
+        self.gvImage.setScene(scene)
